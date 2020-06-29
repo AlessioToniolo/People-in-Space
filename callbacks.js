b67b932d70789f137428f682c3c62dc55b96afe3
Alessio Toniolo <44009231+AlessioToniolo@users.noreply.github.com>
2020-06-29 12:23:14 -0400

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -1,5 +1,5 @@
-const astrosUrl = 'https://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+const astrosUrl = 'http://api.open-notify.org/astros.json';
+const wikiUrl = 'http://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
